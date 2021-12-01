eaac62af51c01686b2e26fad419f8d2d4e027232
Sera <sera.wong28@gmail.com>
2021-12-01 02:14:35 -0500

    update 2nd page

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
    return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
