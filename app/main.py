4eb2a7ce717dd29904f0d1a3c40cd11fb14ac7c9
Sera <sera.wong28@gmail.com>
2021-12-03 04:14:23 -0500

    page4

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -10,3 +10,27 @@ def index():
 @app.route('/page2')
 def page2():
     return render_template('page2.html')
+
+@app.route('/page3')
+def page3():
+    return render_template('page3.html')
+
+@app.route('/page4')
+def page4():
+    return render_template('page4.html')
+
+@app.route('/form', methods=["POST"])
+def form():
+    first_name= request.form.get("first_name")
+    last_name= request.form.get("last_name")
+    email= request.form.get("email")
+    age= request.form.get("age")
+    cereal= request.form.getlist("cereal")
+    dealbreaker= request.form.get("dealbreaker")
+
+    if int(age) > 499:
+        return render_template('old.html')
+    elif len(cereal) < 4 or dealbreaker == None:
+        return render_template('noentry.html')
+    else:
+        return render_template('form.html', first_name=first_name, last_name=last_name, email=email, age=age,cereal=cereal,dealbreaker=dealbreaker)
